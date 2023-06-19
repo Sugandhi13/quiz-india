dbc23201b6ec2360a3334f2e6f4e56c6e40be5e3
Sugandhi13 <>
2023-06-19 22:49:19 +0000

    added javascript and run quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,42 @@
-let yname = document.getElementById(username)[1].value;
-console.log(yname);
+document.addEventListener("DOMContentLoaded", function () {
+
+    const params = new URLSearchParams(window.location.search);
+    document.getElementById("hello-area").innerHTML = params.get('yname');
+    document.getElementById("level-area").innerHTML = params.get('level');
+    document.getElementById("score-area").innerHTML = '0';
+
+    let choice = params.get('level');
+
+    runQuiz(choice);
+
+});
+
+function runQuiz(level) {
+
+    easyQuestions();
+
+}
+
+function easyQuestions() {
+    let easyQuest = [
+        {
+            question: "Which wonder of the world is in India?",
+            answers: {
+                a: 'Machu Picchu',
+                b: 'The Colosseum',
+                c: 'Taj Mahal',
+                d: 'The Great Wall'
+            },
+            correctAnswer: 'c: Taj Mahal'
+        },
+    ];
+
+    for (i = 0; i < easyQuest.length; i++) {
+        displayQuestion(easyQuest[i]);
+    }
+}
+
+function displayQuestion(quest) {
+    document.getElementById("question").innerHTML = quest.question;
+
+}
